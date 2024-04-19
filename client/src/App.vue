8b2511004ddd861671f3197ab922fb923121a72a
ageiser26 <ageiser@prograde.com>
2024-04-19 12:01:57 -0400

    General Cleanup

--- client/src/App.vue
+++ client/src/App.vue
@@ -54,7 +54,6 @@ function addMessage() {
         <Transition :duration="550">
           <div class="chat-box "  :class="store.expandedChat ? 'expanded' : 'compact'">
             <div class="chat-box-header">
-
               <span class="chat-box-toggle" @click="toggleChatWindow()"> <font-awesome-icon :icon="['fas', 'close']" /></span>
               <span class="chat-box-toggle" @click="toggleExpanded()" v-show="!store.expandedChat"> <font-awesome-icon :icon="['fas', 'window-maximize']" /></span>
               <span class="chat-box-toggle" @click="toggleExpanded()" v-show="store.expandedChat"> <font-awesome-icon :icon="['fas', 'window-minimize']" /></span>
@@ -62,14 +61,6 @@ function addMessage() {
             <div class="chat-box-body">
               <div class="chat-box-overlay">
               </div>
-              <!-- Loading
-              <div class="chat-logs" v-show="!store.messages.length" style="text-align: center">
-                <label>
-                  <img src="@/assets/X-1.png" class="pulse" style="width: 65px;"><br>
-                  Connecting...</label>
-              </div>
-               v-show="store.messages.length"
-              -->
                 <div id="chat-logs" class="chat-logs">
                   <div
                       class="chat-msg"
@@ -83,7 +74,6 @@ function addMessage() {
                    <vue-markdown class="cm-msg-text" :source="chat.output"></vue-markdown>
 
                   </div>
-
                   <div
                       class="chat-msg"
                       v-show="store.loading"
@@ -91,7 +81,6 @@ function addMessage() {
                     <span class="msg-avatar">
                       <img class="pulse" src="@/assets/logo.png">
                     </span>
-
                   </div>
 
                 </div>
@@ -101,78 +90,12 @@ function addMessage() {
                 <input type="text" v-model="value"  id="chat-input"  :class="store.expandedChat ? 'expanded' : 'compact'" placeholder="Send a message..."/>
                 <button  class="chat-submit" id="chat-submit" :class="store.expandedChat ? 'expanded' : 'compact'"  v-show="!store.loading"  @click="addMessage"> <font-awesome-icon :icon="['fas', 'paper-plane']" /></button>
                 <button  class="chat-submit" id="chat-submit" :class="store.expandedChat ? 'expanded' : 'compact'"  v-show="store.loading" disabled style="opacity: 0.5"> <font-awesome-icon :icon="['fas', 'paper-plane']" /></button>
-
             </div>
           </div>
         </Transition>
       </section>
     </div>
 
-    <!--
-        <transition name="fade" mode="out-in">
-          <section class="main" v-show="store.messages.length">
-            <ul class="chat-list">
-              <li
-                  class="chat"
-                  v-for="chat in store.messages"
-                  :key="chat.threadId"
-                  :class="chat.role"
-              >
-                <div class="view ">
-                  <div style="display: inline-block; vertical-align: top; text-align: right; width: 6%;">
-                    <img v-show="chat.role != 'user'" src="@/assets/X-1.png" style="width: 25px; background-color: #fff; padding: 6px; border-radius: 180px; filter: drop-shadow(1px 2px 3px rgb(221, 221, 221)); vertical-align: top;">
-                  </div>
-                  <div style="display: inline-block; width: 85%; padding: 0 10px;">
-                    <label>
-                      <strong style="font-size: 14px; font-weight: bold;" v-show="chat.role != 'user'">Assistant</strong>
-                      <strong style="font-size: 14px; font-weight: bold;"
-                              v-show="chat.role === 'user'">You</strong><br>{{ chat.output }}</label>
-                  </div>
-                </div>
-              </li>
-              <li
-                  class="chat"
-                  v-show="store.loading"
-              >
-                <div style="display: inline-block; vertical-align: top; text-align: right; width: 6%;">
-                  <img src="@/assets/X-1.png" class="pulse" style="width: 25px; background-color: #fff; padding: 6px; border-radius: 180px; filter: drop-shadow(1px 2px 3px rgb(221, 221, 221)); vertical-align: top;">
-                </div>
-                <div style="display: inline-block; width: 85%; padding: 0 10px;">
-                  <strong style="font-size: 14px; font-weight: bold; opacity: .25;">Assistant</strong><br/>
-                </div>
-              </li>
-            </ul>
-
-          </section>
-        </transition>
-        <transition name="fade" mode="out-in">
-          <section class="footer">
-            <div style="text-align: right;width: 75%; display: inline-block">
-              <input v-model="value" style=""/>
-
-            </div>
-            <div style=" width: 25%; display: inline-block; text-align: left;">
-              <button type="submit" v-show="!store.loading" style="margin-left: 10px" @click="addMessage">Submit</button>
-              <button type="submit" v-show="store.loading" style="opacity: 0.7;margin-left: 10px" disabled="true">Submit
-              </button>
-            </div>
-          </section>
-        </transition>
-        <transition name="fade" mode="out-in">
-          <section class="main" v-show="!store.messages.length">
-
-            <ul class="chat-loading">
-              <li>
-                <div class="view">
-                  <label>
-                    <img src="@/assets/X-1.png" style="width: 65px;"><br>
-                    Connecting...</label>
-                </div>
-              </li>
-            </ul>
-          </section>
-        </transition>
-        !-->
   </section>
 </template>
 
